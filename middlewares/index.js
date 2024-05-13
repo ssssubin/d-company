3ae03705277653dc357902106525dfe7657a022e
[IoT3] 조수빈 <cdw3521@naver.com>
2024-05-13 22:53:05 +0900

    미들웨어들 exports 해주는 코드 작성

---
+++ middlewares/index.js
@@ -0,0 +1,5 @@
+const checkIdentification = require("./checkIdentification");
+const checkLuggage = require("./checkLuggage");
+const checkRegistered = require("./checkRegisteredVisitor");
+
+module.exports = { checkIdentification, checkLuggage, checkRegistered };
